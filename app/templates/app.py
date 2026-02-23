a18f93937e69518cfac03bd7c4b72b366a008c38
onkarshinde77 <shindeonkar707@gmail.com>
2026-02-23 21:27:54 +0530

    index & live camera page UI improve

--- app/templates/app.py
+++ app/templates/app.py
@@ -414,4 +414,4 @@ def check_video_status(video_id):
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
